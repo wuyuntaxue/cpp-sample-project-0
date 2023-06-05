8f6dee24ee2b4270500683c1725d55329fbff700
taxue <taxue@wuyun.com>
2023-06-05 18:33:29 +0800

    protobuf add armhf build

--- toolchains/armhf.cmake
+++ toolchains/armhf.cmake
@@ -6,6 +6,7 @@ set(CMAKE_CXX_COMPILER "arm-none-linux-gnueabihf-g++")
 
 link_directories("${PROJECT_SOURCE_DIR}/3rd/yaml-cpp/lib/armhf/")
 link_directories("${PROJECT_SOURCE_DIR}/3rd/sqlite3-3420000/armhf/lib/")
+link_directories("${PROJECT_SOURCE_DIR}/3rd/protobuf-v3.20.2/armhf/lib/")
 #添加库文件的查找路径
 
 set(EXECUTABLE_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/bin/armhf)
